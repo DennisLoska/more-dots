7b7833cf9b7f23a16f12c8c673b1718e141ad28b
Dennis Loska <dennis.loska@doctolib.com>
2025-01-14 09:53:35 +0100

    format: scripts

--- scripts/pr_helper.sh
+++ scripts/pr_helper.sh
@@ -6,54 +6,52 @@ BRANCH=$(git branch --show-current)
 PS3="Select the PR kind (1-2): "
 PR_KIND=("regular" "draft")
 
-select KIND in "${PR_KIND[@]}" 
-do
-    case $KIND in
-        "regular")
-            DRAFT=""
-            break
-            ;;
-        "draft")
-            DRAFT="-d"
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
+select KIND in "${PR_KIND[@]}"; do
+	case $KIND in
+	"regular")
+		DRAFT=""
+		break
+		;;
+	"draft")
+		DRAFT="-d"
+		break
+		;;
+	*) echo "invalid option $REPLY" ;;
+	esac
 done
 
 echo ""
 PS3="Select a PR type (1-8): "
 TYPES=("feat" "fix" "test" "chore" "ci" "docs" "refactor" "style")
 
-select TYPE in "${TYPES[@]}"
-do
-    case $TYPE in
-        "feat")
-            break
-            ;;
-        "fix")
-            break
-            ;;
-        "test")
-            break
-            ;;
-        "chore")
-            break
-            ;;
-        "ci")
-            break
-            ;;
-        "docs")
-            break
-            ;;
-        "refactor")
-            break
-            ;;
-        "style")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
+select TYPE in "${TYPES[@]}"; do
+	case $TYPE in
+	"feat")
+		break
+		;;
+	"fix")
+		break
+		;;
+	"test")
+		break
+		;;
+	"chore")
+		break
+		;;
+	"ci")
+		break
+		;;
+	"docs")
+		break
+		;;
+	"refactor")
+		break
+		;;
+	"style")
+		break
+		;;
+	*) echo "invalid option $REPLY" ;;
+	esac
 done
 
 echo ""
@@ -66,26 +64,24 @@ TITLE=$(echo $LAST_COMMIT | sed "s/^.*:/$TYPES($BRANCH):/")
 echo "PR title: $TITLE"
 echo ""
 
-
 PS3="Adjust PR title? (1-2): "
 ADJUST=("yes" "no")
 
-select ADJ in "${ADJUST[@]}" 
-do
-    case $ADJ in
-        "yes")
-            echo "$TITLE" > /tmp/pr_title
-            echo "this is it $(cat /tmp/pr_title)"
-            vim -c 'startinsert' /tmp/pr_title
-            TITLE=$(cat /tmp/pr_title)
-            rm /tmp/pr_title
-            break
-            ;;
-        "no")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
+select ADJ in "${ADJUST[@]}"; do
+	case $ADJ in
+	"yes")
+		echo "$TITLE" >/tmp/pr_title
+		echo "this is it $(cat /tmp/pr_title)"
+		vim -c 'startinsert' /tmp/pr_title
+		TITLE=$(cat /tmp/pr_title)
+		rm /tmp/pr_title
+		break
+		;;
+	"no")
+		break
+		;;
+	*) echo "invalid option $REPLY" ;;
+	esac
 done
 
 echo "PR title: $TITLE"
@@ -99,4 +95,4 @@ BODY=$(cat /tmp/input.$$)
 rm /tmp/input.$$
 
 # send it
-gh pr create $DRAFT --title "$TITLE" --body "$BODY"
+gh pr create "$DRAFT" --title "$TITLE" --body "$BODY"
