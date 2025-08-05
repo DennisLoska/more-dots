793db84908a66d80b890acf957eab889ce754c78
Dennis Loska <dennis.loska@doctolib.com>
2025-08-05 11:02:43 +0200

    update: tmux config

--- scripts/tmux_garden.sh
+++ scripts/tmux_garden.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 SESSION_NAME="garden"
+PROJECT=$1
 
 tmux new-session -d -s $SESSION_NAME -n "neovim"
-tmux send-keys -t $SESSION_NAME:0 "cd $PWD" C-m
-tmux send-keys -t $SESSION_NAME:0 "nvim" C-m
+tmux send-keys -t $SESSION_NAME:1 "cd ~/work/$PROJECT" C-m
+tmux send-keys -t $SESSION_NAME:1 "nvim" C-m
 
-tmux new-window -t $SESSION_NAME:1 -n "process"
-tmux send-keys -t $SESSION_NAME:1 "cd $PWD" C-m
+tmux new-window -t $SESSION_NAME:2 -n "process"
+tmux send-keys -t $SESSION_NAME:2 "cd ~/work/$PROJECT" C-m
+tmux send-keys -t $SESSION_NAME:2 "clear" C-m
 
-tmux new-window -t $SESSION_NAME:2 -n "terminal"
-tmux send-keys -t $SESSION_NAME:2 "cd $PWD" C-m
+tmux new-window -t $SESSION_NAME:3 -n "terminal"
+tmux send-keys -t $SESSION_NAME:3 "cd ~/work/$PROJECT" C-m
+tmux send-keys -t $SESSION_NAME:3 "clear" C-m
 
-tmux attach-session -t $SESSION_NAME:0
+tmux attach-session -t $SESSION_NAME:1
